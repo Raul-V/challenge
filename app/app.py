e93fd0cfd207bd6b1e8fade4e13da1cd1305bd03
Raúl Vivanco <rvivanco@dock.mx>
2019-07-25 05:37:52 -0500

    refactor

--- app/app.py
+++ app/app.py
@@ -6,14 +6,6 @@ import os
 
 app = Flask(__name__)
 
-def get_env_variable(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
-
-
 @app.before_first_request
 def create_database_tables():
     #create_database(DB_URI)
@@ -26,19 +18,11 @@ def get_n_queens_solutions():
     solution.save()
     return f'how many queens? {queens_quantity}'
 
-
-POSTGRES_URL = get_env_variable('POSTGRES_URL')
-POSTGRES_USER = get_env_variable('POSTGRES_USER')
-POSTGRES_PASS = get_env_variable('POSTGRES_PASS')
-POSTGRES_DB = get_env_variable('POSTGRES_DB')
-
-DB_URI = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_URL}/{POSTGRES_DB}'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-
 if __name__ == '__main__':
     from db import db
+    from config import config
     db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PROPAGATE_EXCEPTIONS'] = True
     app.run(host='0.0.0.0', debug=False)
